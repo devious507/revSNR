a74cd09f6db5a7d24e26392ce08840b619720994
Paul Oster <paulmoster@gmail.com>
2014-01-23 10:45:14 -0600

    changed refresh to 5 seconds and count to 16

--- autoload.js
+++ autoload.js
@@ -1,4 +1,4 @@
 <script>
-	var auto_refresh = setInterval( function() { submitform(); }, 10000);
+	var auto_refresh = setInterval( function() { submitform(); }, 5000);
 	function submitform() { document.myForm.submit(); }
 </script>
